997e7782481373789ea123d1dc218b37ea0775d2
Naïm Gómez <33960177+NgomezKS@users.noreply.github.com>
2020-06-28 08:34:58 +0100

    d

--- src/VS19QT.cpp
+++
@@ -1,7 +0,0 @@
-#include "VS19QT.h"
-
-VS19QT::VS19QT(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-}
--- src/VueloautnomoparalaaeronaveF4PhantomII.cpp
+++
@@ -1,7 +0,0 @@
-#include "VueloautnomoparalaaeronaveF4PhantomII.h"
-
-VueloautnomoparalaaeronaveF4PhantomII::VueloautnomoparalaaeronaveF4PhantomII(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
-}
