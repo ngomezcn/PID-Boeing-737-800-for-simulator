efdf98131c5229bfaf19ccfc4b1696400642b178
Naïm Gómez <castelldeneim@gmail.com>
2020-09-23 12:55:20 +0200

    Class csv::read

--- Basic-AI-for-X-Plane11/Basic-AI-for-X-Plane11/src/csv/read.cpp
+++ Basic-AI-for-X-Plane11/Basic-AI-for-X-Plane11/src/csv/read.cpp
@@ -2,10 +2,30 @@
 
 using namespace std;
 
-const int lineas = 2;
-const int columnas = 5;
+const int lineas = 5;
+const int columnas = 10;
 
 std::string matrix[lineas][columnas];
+
+void get_dimension(std::string filename)
+{
+    std::ifstream myFile(filename);
+
+    if (!myFile.is_open()) prerror("No se ha podido abrir el archivo: {0}", filename);            
+
+    std::string line;
+    std::string val;
+
+    int a = 0;
+    while (std::getline(myFile, line))
+    {
+        a++;
+    }
+
+    std::cout << a;
+
+    myFile.close();
+}
 
 void read_csv(std::string filename) {
 
@@ -15,7 +35,7 @@ void read_csv(std::string filename) {
     if (!myFile.is_open()) prerror("No se ha podido abrir el archivo: {0}", filename);            
 
     std::string line;
-    std::string val;
+    std::string val;   
 
     // Convierte las lineas en extraidas en crudo e introduce cada valor en la casilla que corresponde dentro del array.
     for (int i = 0; i < lineas; i++)
@@ -24,7 +44,7 @@ void read_csv(std::string filename) {
          
          int indexColums = 0;
 
-         for (int j = 0; j < columnas + (columnas - extraComa); j++)
+         for (int j = 0; j < line.length(); j++)
          {  
              if (line[j] != ',')
              {
@@ -33,18 +53,66 @@ void read_csv(std::string filename) {
              }
          }
 
-         std::cout << "\n";              
     }
-    
+
     myFile.close();
+}
+
+namespace csv
+{
+    class read
+    {
+    public:
+        std::string filename;
+        int columns;
+        int rows = 0;
+
+        read(std::string &_filename)
+        {
+            filename = _filename;
+
+            std::ifstream myFile(filename);
+
+            if (!myFile.is_open())
+            {
+                prerror("No se ha podido abrir el archivo: {0}", filename);
+            }
+
+            std::string line;
+            std::string val;
+
+            while (std::getline(myFile, line))
+            {
+                rows++;
+            }
+
+            myFile.close();
+
+        }
+    };
+
+}
+
+namespace path
+{
+    std::string flightplan = { "test.csv"};
 }
 
 int main() {
 
-  
-    read_csv("tesst.csv");    
+   /* int a = 1;
+    int b = 2;
 
-  /*  for (int i = 0; i < lineas; i++)
+    */
+    csv::read flightplan(path::flightplan);
+
+    std::cout << "F: " << flightplan.rows << " -";
+  
+   // get_dimension("test.csv");
+    read_csv("test.csv");    
+
+    /*std::cout << std::endl;
+    for (int i = 0; i < lineas; i++)
     {
         for (int j = 0; j < columnas; j++)
         {
